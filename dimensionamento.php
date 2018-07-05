e6f3abca101110f9d0b4f1e3578f120dad80ce5e
Anderson de Faria <anderson.faria@intelbras.com.br>
2018-07-05 14:34:41 -0300

    teste

---
+++ dimensionamento.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CamProjetc - Projetos de Segurança</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <h2>CamProject</h2>
+    <h3>Dimensionamento de Projetos de Segurança</h3>
+    <span>Descubra qual o projeto de câmeras ideial para seu estabelecimento.</span>
+    <hr>
+
+    <h3>Seu Projeto</h3>
+
+    <img src="img/exemplo.jpg" width="900">
+
+
+<br>
+    <br>
+    <br>
+    <form class="form-horizontal" action="/index.php">
+        <div class="form-group">
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-default">Voltar</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
