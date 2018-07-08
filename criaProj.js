829b36eb1deb0cd53c95d4fc11f9527990005005
lohan209 <36992808+lohan209@users.noreply.github.com>
2018-07-07 22:32:02 -0300

    adicionado calculo real dist cam

--- criaProj.js
+++ criaProj.js
@@ -10,28 +10,57 @@ function draw() {
   //COORDENADAS PONTA ESQUERDA SUPERIOR ÁREA
   var x = 30;
   var y = 30; 
-  var cordAx = 60;
-  var cordAy = 205;
-  var cordBx = 200;
-  var cordBy = 100;
+  var cordAx = 200;
+  var cordAy = 300;
+  var cordBx = 300;
+  var cordBy = 200;
+
+  var perncetD = 0.01;
+  var percentC = 0.26;
+  var percentR = 0.51;
+  var percentI = 0.76;
+  
+  //informação que o usuário irá inserir
   var larg = 30;
   var prof = 30;
+  var dist = 0; 
+
+  //informações que será pego do BD
+  var resolucao = 1280;
+  var lente = 0.36;
+  var sensor = 0.25;
+  var ang = 80.36
+
 
   //Criando a planta - quadrado ou retangulo
   ctx.strokeStyle = "rgba(0,0,0)";
   ctx.lineWidth = 1;
   ctx.strokeRect(x, y , larg*10, prof*10);
 
+
+  //Cálculo distância da câmera para IDENTIFICAÇÃO
+  var b = lente/sensor;
+  var largObj = (resolucao/(300*percentI));
+  var distT = (largObj*b*0.7); //cateto Oposto de B e adjacente de A
+  var distTOK = parseFloat(distT.toFixed(2));
+
+  var angb = 180 - 90 - (ang/2); //angulo B
+  var angOK = parseFloat(angb.toFixed(2));
+
+  var hipB = distTOK/(Math.sin(angOK)); //hipotenusa é a distancia entre o (x0,y0) e (x,y)
+  var catOpostoA = Math.sqrt((Math.pow(hipB, 2))-(Math.pow(distTOK, 2))); //achando o cateto Adjacente de B / catOposto de A
+  var catOpOK = parseFloat(catOpostoA.toFixed(2));
+
+
   //TRIANGULO IDENTIFICAÇÃO
   ctx.fillStyle = "rgba(255,0,0,0.5)"
   ctx.beginPath();
-  //(x,y) ponta 1
   ctx.moveTo(x+10,y+10);  
-  //(x,y) ponta 3
-  ctx.lineTo(cordAx, cordAy);
-  //(x,y) ponta 3
-  ctx.lineTo(cordBx, cordBy);
+  ctx.lineTo(catOpOK+x+10, distTOK+10+y);
+  ctx.lineTo(distTOK+y+10, catOpOK+x+10);
   ctx.fill();
+
+
 
 
   //ADICIONANDO O QUE É LARGURA E O QUE É PROFUNDIDADE
